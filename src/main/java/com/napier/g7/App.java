6cb1ba89cea9cf3ac1bce80c223762e20989185d
macbookair <40616703@live.napier.ac.uk>
2024-02-15 13:12:56 +0630

    Adding displayTopPopulatedCapitalCitiesInWorld method

--- src/main/java/com/napier/g7/App.java
+++ src/main/java/com/napier/g7/App.java
@@ -1471,4 +1471,61 @@ public class App
         }
     }
 
+    /**
+     * Displays the top N populated capital cities in the world based on population in descending order.
+     *
+     * @param topN The number of top populated capital cities to display.
+     */
+    public ArrayList<Capital> displayTopPopulatedCapitalCitiesInWorld(int topN) {
+        // Check if topN is non-positive
+        if (topN < 0) {
+            System.out.println("Invalid input: topN should be a positive integer.");
+            return new ArrayList<>();
+        }
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT ci.ID, ci.name AS cityName, c.name AS countryName, ci.population " +
+                            "FROM country c " +
+                            "LEFT JOIN city ci ON c.capital = ci.id " +
+                            "ORDER BY ci.population DESC " +
+                            "LIMIT " + topN;
+
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Extract capital city information
+            ArrayList<Capital> capitals = new ArrayList<>();
+            while (rset.next()) {
+                // Create a new Capital object
+                Capital capital = new Capital();
+                // Set capital attributes from the result set
+                capital.setId(rset.getInt("ID"));
+                capital.setName(rset.getString("cityName"));
+                capital.setCountryName(rset.getString("countryName"));
+                capital.setPopulation(rset.getLong("population"));
+                // Add the Capital object to the ArrayList
+                capitals.add(capital);
+            }
+
+            // Check if any capital cities were found
+            if (capitals.isEmpty()) {
+                System.out.println("No capital cities found");
+            } else {
+                // Print the top N populated capital cities in the world
+                printCapitalCities(capitals);
+            }
+            return capitals;
+        } catch (Exception e) {
+            // Print error messages in case of an exception
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get top populated capital cities in the world");
+            return new ArrayList<>();
+        }
+    }
+
+
 }
